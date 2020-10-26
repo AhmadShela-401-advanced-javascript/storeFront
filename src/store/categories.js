b5381fcd4f3d9e3b02f9b4ba328a2321adcc536a
Ahmad Shela <ahmedsh717@gmail.com>
2020-10-26 22:34:44 +0300

    class-02

---
+++ src/store/categories.js
@@ -0,0 +1,30 @@
+let initalState = {    
+    categories: ['cake','drinks','food'],
+    selectedCategory: 'cake'
+};
+// reducer
+export default (state = initalState, action) => {
+    // update the state based on an action
+    // increment, decrement, reset votes
+    let {type, payload} = action;
+    let categories = state.categories;
+    console.log('Cats',categories);
+    switch(type) {
+        case 'SELECT_CATEGORY':
+            // increment a specific candidate votes
+            console.log('payload',payload);
+            initalState.selectedCategory=payload;
+            console.log('initalState.selectedCategory',initalState.selectedCategory);
+            return  {categories,selectedCategory:initalState.selectedCategory};
+        default:
+            return state;
+    }
+}
+// actions
+export const selectCategory =(category) =>{
+    console.log('?????????category',category);
+    return {
+        type: 'SELECT_CATEGORY',
+        payload: category
+    }
+}
