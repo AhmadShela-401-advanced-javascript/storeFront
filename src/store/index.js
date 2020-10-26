d9c7348334280ed12c6bb901fdeb256f76c388be
Ahmad Shela <ahmedsh717@gmail.com>
2020-10-26 22:34:50 +0300

    class-02

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,32 @@
+// // store index.js =-> combine reducers
+// // create and export my store 
+
+// import {createStore, combineReducers} from 'redux';
+
+// // to enable the chrome extension for redux
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import votes from './votes';
+
+// // when you have more than one reducer combine here everything
+// let reducers = combineReducers({votes});
+
+// const store = () => {
+//     return createStore(reducers, composeWithDevTools())
+// }
+
+// export default store();
+
 // store index.js =-> combine reducers
 // create and export my store 
-
 import {createStore, combineReducers} from 'redux';
-
 // to enable the chrome extension for redux
 import { composeWithDevTools } from 'redux-devtools-extension';
-import votes from './votes';
-
+import categories from './categories';
+import products from './products';
+import cart from './cart';
 // when you have more than one reducer combine here everything
-let reducers = combineReducers({votes});
-
+let reducers = combineReducers({categories,products,cart});
 const store = () => {
     return createStore(reducers, composeWithDevTools())
 }
-
 export default store();
