bd0413d16ce9cdce027e4f81dddd017d85049f4f
Ahmad Shela <ahmedsh717@gmail.com>
2020-10-28 00:53:40 +0300

    redux-thunk

--- src/store/categories.js
+++ src/store/categories.js
@@ -1,5 +1,5 @@
 let initalState = {    
-    categories: ['cake','drinks','food'],
+    categories: [],
     selectedCategory: 'cake'
 };
 // reducer
@@ -8,21 +8,25 @@ export default (state = initalState, action) => {
     // increment, decrement, reset votes
     let {type, payload} = action;
     let categories = state.categories;
-    console.log('Cats',categories);
+    // console.log('Cats',categories);
     switch(type) {
         case 'SELECT_CATEGORY':
             // increment a specific candidate votes
-            console.log('payload',payload);
+            // console.log('payload',payload);
             initalState.selectedCategory=payload;
-            console.log('initalState.selectedCategory',initalState.selectedCategory);
+            console.log('!!!payload',payload);
+            // console.log('initalState.selectedCategory',initalState.selectedCategory);
             return  {categories,selectedCategory:initalState.selectedCategory};
+        case 'GET':
+            categories = payload.results || payload.result
+            return {categories}
         default:
             return state;
     }
 }
 // actions
 export const selectCategory =(category) =>{
-    console.log('?????????category',category);
+    // console.log('?????????category',category);
     return {
         type: 'SELECT_CATEGORY',
         payload: category
