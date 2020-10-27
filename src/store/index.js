571d40d3bbeea280b162a0798a3585eac753b245
Ahmad Shela <ahmedsh717@gmail.com>
2020-10-28 00:53:51 +0300

    redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -18,15 +18,16 @@
 
 // store index.js =-> combine reducers
 // create and export my store 
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers,applyMiddleware} from 'redux';
 // to enable the chrome extension for redux
 import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from 'redux-thunk';
 import categories from './categories';
 import products from './products';
 import cart from './cart';
 // when you have more than one reducer combine here everything
 let reducers = combineReducers({categories,products,cart});
 const store = () => {
-    return createStore(reducers, composeWithDevTools())
+    return createStore(reducers, composeWithDevTools(applyMiddleware(ReduxThunk)))
 }
 export default store();
