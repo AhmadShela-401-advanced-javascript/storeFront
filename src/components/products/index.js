478e0fc292a57af3d924d44ae703de8e8241c2dd
Ahmad Shela <ahmedsh717@gmail.com>
2020-10-28 00:53:07 +0300

    redux-thunk

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 // import { selectCategory } from '../../store/products'
 import PropTypes from 'prop-types';
@@ -10,7 +10,8 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { left } from '@popperjs/core';
 import Button from '@material-ui/core/Button';
-import {addToCart} from '../../store/cart'
+import {addToCart} from '../../store/cart';
+import {getRemoteProductData,reduceStockQuantity} from '../../store/actions'
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -61,27 +62,43 @@ const Products = props => {
         props.selectCategory(event.target.textContent)
         setValue(newValue);
     };
-    console.log('props in product', props);
+    // console.log('props in product', props);
     // let selectedCategory = props.selectCategory;
     // function handleCategoryClick(e) {
     //     // console.log(e.target.textContent);
     //     // selectedCategory = e.target.textContent
     //     selectCategory(e.target.textContent)
     // }
+    const handleAddToCart = (product) =>{
+        console.log('Add To Cart Product ===== :',product);
+        if(product.inStock > 0){
+            props.addToCart(product);
+            props.reduceStockQuantity(product,product._id);
+            // props.getRemoteProductData();
+        }else{
+            console.log('Add To Cart Product in Stock ===== :',product.inStock);
+            alert('No available Items left')
+        }
+
+    }
+    useEffect(() => {
+        console.log('I am Working {Products} !!!!!!!!!!!!!!!!');
+        props.getRemoteProductData()
+       },[]);
     return (
         <>
             <div className={classes.root}>
                 <div style={{ display: 'flex' }}>
                     {props.products.map((product, idx) => {
-                        if (product.cat == props.selectedCategory) {
-                            console.log('>>>>>>>>>>>>>', props.selectedCategory);
+                        if (product.category == props.selectedCategory) {
+                            console.log('>>>>>>props.selectedCategory>>>>>>>', props.selectedCategory);
                             return (
                                 <div key={idx} style={{ border: '1px solid black', width: 'fit-content', padding: '10px', margin: '10px' }}>
                                     <h3 id='productName'>{product.name}</h3>
                                     <img id='productImg' src={`${product.img}`} style={{ width: '15rem' }}></img>
-                                    <p>in stok : <strong>{product.inStok}</strong></p>
+                                    <p>in stok : <strong>{product.inStock}</strong></p>
                                     <p>price : <strong id='producPrice'>{product.price}</strong></p>
-                                    <Button onClick={()=>{props.addToCart(product)}} variant="contained" color="primary">Order</Button>
+                                    <Button onClick={()=>{handleAddToCart(product)}} variant="contained" color="primary">Order</Button>
                                 </div>
                             )
                         }
@@ -99,6 +116,6 @@ const mapStateToProps = state => (
 }
 );
 
-const mapDispatchToProps = { addToCart };
+const mapDispatchToProps = { addToCart,getRemoteProductData,reduceStockQuantity };
 // no need to add dispatch part (no actions)
 export default connect(mapStateToProps,mapDispatchToProps)(Products);
