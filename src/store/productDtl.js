a4bf15b5c64608efa7ef3425f425f37f8d19ee37
Ahmad Shela <ahmedsh717@gmail.com>
2020-10-30 21:20:06 +0200

    RKT

--- src/store/productDtl.js
+++ src/store/productDtl.js
@@ -2,10 +2,12 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const productDtlSlice = createSlice({
     name:'product',
-    initialState : {},
+    initialState:{} ,
     reducers: {
         selectProduct(state, action) {
-            state = action.payload
+            console.log('63521563413521354351',action.payload);
+            state.product = action.payload
+            console.log('63521563\\state//413521354351',state);
         }
     }
 });
